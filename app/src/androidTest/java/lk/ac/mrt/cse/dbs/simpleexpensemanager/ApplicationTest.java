f52b8a6587e24ffc4ae315b6c01089afda071697
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-05-08 14:05:08 +0530

    added test cases for check database

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -19,11 +19,65 @@ package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import java.util.Date;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    private PersistentAccountDAO accountDAO;
+    private PersistentTransactionDAO transactionDAO;
+    private final String DB_NAME = "test_expense";
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        accountDAO = new PersistentAccountDAO(getContext(), DB_NAME);
+        transactionDAO = new PersistentTransactionDAO(getContext(), DB_NAME);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        accountDAO.close();
+        transactionDAO.close();
+        getContext().deleteDatabase(DB_NAME);
+        super.tearDown();
+    }
+
+    public void testInsertAccount() throws InvalidAccountException {
+        //Insert An account
+        Account account = new Account("A0001","ABC Bank", "Ishad-M-I-M", 1000.00);
+        accountDAO.addAccount(account);
+
+        //get the account back and check
+        Account checkAccount = accountDAO.getAccount("A0001");
+        assertEquals("ABC Bank", checkAccount.getBankName());
+        assertEquals("Ishad-M-I-M", checkAccount.getAccountHolderName());
+        assertEquals(1000.00, checkAccount.getBalance());
+    }
+
+    public void testInsertTransaction() {
+        //Insert an transaction
+        Date today = new Date();
+        transactionDAO.logTransaction(today, "A0001", ExpenseType.EXPENSE, 1000.00);
+
+        //check transaction
+        List<Transaction> transactions = transactionDAO.getAllTransactionLogs();
+        Transaction checkTransaction = transactions.get(0);
+        assertEquals("A0001", checkTransaction.getAccountNo());
+        assertEquals(ExpenseType.EXPENSE, checkTransaction.getExpenseType());
+        assertEquals(1000.00, checkTransaction.getAmount());
     }
 }
